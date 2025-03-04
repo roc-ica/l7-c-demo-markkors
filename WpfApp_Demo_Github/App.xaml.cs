eb5f8e7d7f5a4048ae57912fd32a6d1870567b66
Mark Kors <m.kors@rocmn.nl>
2025-03-04 12:29:28 +0100

    Add project files.

---
+++ WpfApp_Demo_Github/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_Demo_Github;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
